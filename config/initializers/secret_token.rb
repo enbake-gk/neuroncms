a1c4954cc06e07632abdfe401f706b1df347033e
Gagan <gkumar@enbake.com>
2014-07-14 12:09:55 +0530

    launch it

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AcmeCms::Application.config.secret_token = '823a78c09dbcfa15a38eda66b72f164a0741cced85e088080ad59724d58a732cae4fa9d2756727bfc20e1b111f84e79d5661985649d506971617bb86ed678ae4'
