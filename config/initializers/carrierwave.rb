a6f23c226cfd6ff0c0848dc5fbd3ca02c6507b09
Gagan <gkumar@enbake.com>
2014-07-18 16:20:30 +0530

    launch it

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,13 +11,15 @@ CarrierWave.configure do |config|
   when :production
     # the following configuration works for Amazon S3
     config.storage          = :fog
-    config.fog_credentials  = {
-      provider:                 'AWS',
-      aws_access_key_id:        ENV['S3_KEY_ID'],
-      aws_secret_access_key:    ENV['S3_SECRET_KEY'],
-   
-    }
-    config.fog_directory    = ENV['S3_BUCKET']
+    config.fog_credentials = {
+    :provider               => 'AWS',                        
+    :aws_access_key_id      => "AKIAJMV6IAIXZQJJ2GHQ",    
+    :aws_secret_access_key  => "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" 
+  }
+  config.fog_directory  = "com.neuronicgames.neuroncms/content"                  
+  config.fog_public     = false                                   
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  
+  config.fog_authenticated_url_expiration = 604800
 
   else
     # settings for the local filesystem
@@ -27,3 +29,22 @@ CarrierWave.configure do |config|
 
 end
 
+# CarrierWave.configure do |config|
+#   config.fog_credentials = {
+#     :provider               => 'AWS',                        
+#     :aws_access_key_id      => "AKIAJYXFJGF3O4D52GTQ",    
+#     :aws_secret_access_key  => "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" 
+#   }
+#   config.fog_directory  = "com.neuronicgames.neuroncms/content"                  
+#   config.fog_public     = false                                   
+#   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  
+#   config.fog_authenticated_url_expiration = 604800
+# end
+
+
+
+
+# S3_KEY:                       AKIAJMV6IAIXZQJJ2GHQ
+# S3_PATH:                      com.neuronicgames.neuroncms/content
+# S3_PROTOCOL:                  https
+# S3_SECRET:                    qwX9pSUr8vD+CGHIP1w4tYEpWV6dsK3gSkdneY/V
