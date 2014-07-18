cda53261e76a455963d4bf27da8cd6e50fb2df46
Gagan <gkumar@enbake.com>
2014-07-18 18:40:08 +0530

    launch it

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,6 @@
 CarrierWave.configure do |config|
-
+  config.permissions = 0666
+  config.directory_permissions = 0777
   config.cache_dir = File.join(Rails.root, 'tmp', 'uploads')
 
   case Rails.env.to_sym
