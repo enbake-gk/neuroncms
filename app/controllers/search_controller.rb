f5c89de1d9420dd4af9e99a945569c123fd0c216
Mayank Mishra <mmishra@enbake.com>
2014-11-11 19:18:35 +0530

    code cleanup for nested json api

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -25,15 +25,10 @@ class SearchController < Locomotive::Api::BaseController
 
 
 	def nested  
-		# begin
-
+		begin
+			# Find model name in contentty
 			@locomotive_model = Locomotive::ContentType.where({ slug: /^.*#{params[:model]}.*$/i } ).first  
-		   
-		
-
 		    if @locomotive_model.present?
-			    value_arr = []
-			    nested_model = {}
 			    final_object = []
 
 		    	if params[:id].present?
@@ -43,22 +38,6 @@ class SearchController < Locomotive::Api::BaseController
 		        end
 			    # iterate each entries
 				@entries.each do |entries|
-			            # iterate each entries with key and value 
-						# entries.attributes.each_pair do |key, value|
-						# 	# check if array is exists in entry
-						# 		if value.is_a?(Array) && key.include?("_ids")
-						# 			new_key = key.gsub('_ids', 's')
-						# 			   # itereate array with key and value
-					 #                    value.each_with_index  do |item, index|
-					 #                   	# Find each nested record
-						#                    value_arr << Locomotive::ContentEntry.where({ :id => item})
-						#                    nested_model.merge!({new_key.to_s => value_arr})
-					 #                    end
-					 #                    value_arr =[]	
-				  #                   # nested_model.merge!({new_key.to_s => value_arr})
-						# 	   end
-					 #    end	
-					    
                         entries["custom_fields_recipe"]["rules"].each do |recipe|
                           recipe.each_pair do |k, v|
                           	# Replace file(image) name  with file_url(location)
@@ -68,41 +47,24 @@ class SearchController < Locomotive::Api::BaseController
                             end
                              # End Replace file(image) name  with file_url(location)
                                
-                             # Find if belongs_to assosiation is defined in custom_fiels_recipe 
-                             if(v == "belongs_to")
+                             # Find if any assosiation is defined in custom_fiels_recipe 
+                             if((v == "belongs_to") || (v == "many_to_many") || (v == "has_many") || (v == "has_one"))
+                            	# append object in clas name
                             	entries.attributes.merge!(recipe["name"] => entries.send(recipe["name"]))
                              end
-                             # End Finding if belongs_to assosiation is defined in custom_fiels_recipe 
-
-                             # Find if many to many assosiation is defined in custom_fiels_recipe 
-                             if(v == "many_to_many")
-                            	entries.attributes.merge!(recipe["name"] => entries.send(recipe["name"]))
-                             end
-                             # End Finding if many to many assosiation is defined in custom_fiels_recipe 
-
-                             # Find if has many assosiation is defined in custom_fiels_recipe 
-                             if(v == "has_many")
-                            	entries.attributes.merge!(recipe["name"] => entries.send(recipe["name"]))
-                             end
-                             # End Finding if many to many assosiation is defined in custom_fiels_recipe 
-
-                             # Find if has one assosiation is defined in custom_fiels_recipe 
-                             if(v == "has_one")
-                            	entries.attributes.merge!(recipe["name"] => entries.send(recipe["name"]))
-                             end
-                             # End Finding if has many assosiation is defined in custom_fiels_recipe 
+                             # End Finding if assosiation is defined in custom_fiels_recipe 
                           end
                         end
-                       
 					    entries.attributes.delete("custom_fields_recipe")
-					    # entries.attributes.merge!(nested_model) if nested_model.present?
-			            # final_object.merge!(entries.attributes)
 			            final_object << entries.attributes
 			    end
 		  	end 
-	    # rescue
-		    # final_object = 'Invalid Request! Please check and try again'
-		# end
+	    rescue
+		    final_object = 'Invalid Request! Please check and try again'
+		end
+
+		final_object = 'No record found for model  '+params[:model]  unless @locomotive_model.present?
+
 		respond_to do |format|
 		       format.json { render json: final_object }
 		end
